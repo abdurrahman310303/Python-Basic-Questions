21ba5b802b912e2f4acbfed8f835c0742620aafe
abdurrahman310303 <abdurrahman310303@gmail.com>
2025-01-01 11:45:00 +0500

    Implement arithmetic operations

---
+++ 03_variables_and_operators/arithmetic.py
@@ -0,0 +1,18 @@
+num1 = 15
+num2 = 4
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+floor_division = num1 // num2
+modulus = num1 % num2
+exponentiation = num1 ** num2
+
+print(addition)
+print(subtraction)
+print(multiplication)
+print(division)
+print(floor_division)
+print(modulus)
+print(exponentiation)
