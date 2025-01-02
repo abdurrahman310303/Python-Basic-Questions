1a7f4e529b4242e6f39c0806388c9d078abc9ef9
abdurrahman310303 <abdurrahman310303@gmail.com>
2025-01-02 08:15:00 +0500

    Implement for and while loops

---
+++ 04_control_structures/loops.py
@@ -0,0 +1,22 @@
+for i in range(5):
+    print(i)
+
+fruits = ["apple", "banana", "orange"]
+for fruit in fruits:
+    print(fruit)
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(f"{i} is even")
+    else:
+        print(f"{i} is odd")
+
+count = 0
+while count < 5:
+    print(f"Count: {count}")
+    count += 1
+
+number = 1
+while number <= 10:
+    print(number * number)
+    number += 1
